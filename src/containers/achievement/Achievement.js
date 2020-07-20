08264ca00329be48246722d0faa07e9cca4b975c
keremcanb <keremcan@gmail.com>
2020-07-21 02:53:30 +0300

    img

--- src/containers/achievement/Achievement.js
+++ src/containers/achievement/Achievement.js
@@ -10,34 +10,32 @@ export default function Achievement() {
     win.focus();
   }
   return (
-    <Fade bottom duration={1500}>
-      <div className='main' id='achievements'>
-        <div className='achievement-main-div'>
-          <div className='achievement-header'>
-            <h1 className='heading achievement-heading'>
-              {achievementSection.title}
-            </h1>
-            <p className='subTitle achievement-subtitle'>
-              {achievementSection.subtitle}
-            </p>
-          </div>
-          <div className='achievement-cards-div'>
-            {achievementSection.achivementsCards.map((card) => {
-              return (
-                <AchivementCard
-                  cardInfo={{
-                    title: card.title,
-                    description: card.subtitle,
-                    thumbnail: card.thumbnail,
-                    image: card.image,
-                    footer: card.footerLink,
-                  }}
-                />
-              );
-            })}
-          </div>
+    <div className='main' id='achievements'>
+      <div className='achievement-main-div'>
+        <div className='achievement-header'>
+          <h1 className='heading achievement-heading'>
+            {achievementSection.title}
+          </h1>
+          <p className='subTitle achievement-subtitle'>
+            {achievementSection.subtitle}
+          </p>
+        </div>
+        <div className='achievement-cards-div'>
+          {achievementSection.achivementsCards.map((card) => {
+            return (
+              <AchivementCard
+                cardInfo={{
+                  title: card.title,
+                  description: card.subtitle,
+                  thumbnail: card.thumbnail,
+                  image: card.image,
+                  footer: card.footerLink,
+                }}
+              />
+            );
+          })}
         </div>
       </div>
-    </Fade>
+    </div>
   );
 }
